c371ab22a99bfcddff3f0a79d1e06f5aea147840
AB75359 <AB75359@DNB25150.dnbnor.net>
2017-04-22 12:41:55 +0200

    ok

---
+++ Selenium/src/test/java/features/testSum/TestSumExecution.java
@@ -0,0 +1,14 @@
+package features.testSum;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features/")
+
+public class TestSumExecution
+{
+
+}
---
+++ Selenium/src/test/java/features/testSum/TestSumStepDefinationFile.java
@@ -0,0 +1,43 @@
+package features.testSum;
+
+import org.junit.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import utility.UtilityClass;
+
+public class TestSumStepDefinationFile
+{
+  public  TestSumData testDataSum;
+  public UtilityClass utilityClass;
+  
+  public TestSumStepDefinationFile()
+  {
+    this.testDataSum = new TestSumData();
+    this.utilityClass=new UtilityClass();
+  }
+ 
+  
+  @Given("^two numbers are (\\d+) and (\\d+)$")
+  public void testData(int arg1, int arg2) 
+  {
+    testDataSum.setA(arg1);
+    testDataSum.setB(arg2);
+    System.out.println("Test data has been initilized successfully");
+  }
+
+  @When("^I will add two numbers$")
+  public void i_will_add_two_numbers() 
+  {
+    testDataSum.setResult(utilityClass.sum(testDataSum.getA(), testDataSum.getB()));
+  }
+
+  @Then("^the output should be (\\d+)$")
+  public void VrifyOP(int arg1) 
+  {
+    Assert.assertEquals(arg1, testDataSum.getResult());
+  }
+
+
+}
