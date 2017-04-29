c0ac145da2d3ed8fd2d1135050a79ec46967f0c8
ab75359 <ab75359@W7x64-D120-093.udnbnor.net>
2017-04-29 22:43:41 +0200

    Adding excel driven code

--- Selenium/src/test/java/runner/TestMain.java
+++ Selenium/src/test/java/runner/TestMain.java
@@ -1,5 +1,11 @@
 package runner;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
 import org.junit.runner.JUnitCore;
 
 import cucumberTest.testMul.TestMulExecution;
@@ -7,13 +13,42 @@ import cucumberTest.testSum.TestSumExecution;
 
 public class TestMain
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		JUnitCore jUnitCore = new JUnitCore();
-
-		jUnitCore.run(TestSumExecution.class);
-		jUnitCore.run(TestMulExecution.class);
-
+		HSSFWorkbook workbook = null;
+		String driverFilePath = System.getProperty("user.dir") + "\\src\\test\\resources\\Driver.xls";
+		FileInputStream xlFile = new FileInputStream(driverFilePath);
+		workbook = new HSSFWorkbook(xlFile);
+		HSSFSheet sheet = workbook.getSheetAt(0);
+		int firstRow = sheet.getFirstRowNum();
+		// in POI -- row number starts from 0
+		int lastRow = sheet.getLastRowNum();
+		int totalRows = lastRow - firstRow + 1;
+		for (int k = 1; k < totalRows; k++)
+		{
+			Row currentRow = sheet.getRow(k);
+			String moduleName = currentRow.getCell(1).getStringCellValue().trim().toUpperCase();
+			String status=currentRow.getCell(0).getStringCellValue().trim().toUpperCase();
+			switch (moduleName) {
+			case "TESTSUMEXECUTION":
+				if (status.equals("Y"))
+				{
+					System.out.println("Executing SUM Module");
+					jUnitCore.run(TestSumExecution.class);
+				}
+				break;
+			case "TESTMULEXECUTION":
+				if (status.equals("Y"))
+				{
+					System.out.println("Executing Mul Module");
+					jUnitCore.run(TestMulExecution.class);
+				}				
+				break;
+			default:
+				break;
+			}
+		}
+		workbook.close();
 	}
-
 }
