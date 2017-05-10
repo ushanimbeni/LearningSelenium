b257f8471355ccf8fbdb854cd7061762b34ecfb6
ab75359 <ab75359@W7x64-D120-093.udnbnor.net>
2017-05-10 20:52:47 +0200

    ok

--- Selenium/src/test/java/runner/TestMain.java
+++ Selenium/src/test/java/runner/TestMain.java
@@ -37,7 +37,7 @@ public class TestMain
 				{
 					System.out.println("Executing SUM Module");
 					jUnitCore.run(TestSumExecution.class);
-					//syso11
+					//sysoDeepak
 				}
 				break;
 			case "TESTMULEXECUTION":
